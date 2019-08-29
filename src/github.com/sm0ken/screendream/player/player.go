c495a19dda5b57460dd2ce591be5fce41acca544
crash <crash@crashc.at>
2019-08-29 23:51:31 +0200

    added SkipTo to player

--- src/github.com/sm0ken/screendream/player/player.go
+++ src/github.com/sm0ken/screendream/player/player.go
@@ -2,10 +2,10 @@ package player
 
 import (
 	v "github.com/sm0ken/screendream/video"
+	"io"
 	e "os/exec"
+	"strconv"
 )
-
-
 
 type status int
 
@@ -15,15 +15,13 @@ const (
 	dead
 )
 
-
 var command *e.Cmd
-var playerstatus status
-
+var playerstatus status = dead
 
 //plays the video passed as vid
-func Play(vid *v.V){
+func Play(vid *v.V) {
 	//kill other playing videos
-	if playerstatus != dead && command!=nil{
+	if playerstatus != dead && command != nil {
 		Kill()
 	}
 	//start vid
@@ -32,7 +30,17 @@ func Play(vid *v.V){
 }
 
 // kills the currently playing video
-func Kill(){
+func Kill() {
 	command.Process.Kill()
 	command = nil
+}
+
+func SkipTo(time int) {
+	if command != nil && playerstatus != dead {
+		stdin, err := command.StdinPipe()
+		defer stdin.Close()
+		if err != nil {
+			io.WriteString(stdin, "seek "+strconv.Itoa(time)+" 2\n")
+		}
+	}
 }
