331fe29c0903cdee886428bb96342a195fd62440
Fuyu Kanoe <zero@hosthost.local>
2019-09-04 20:17:22 +0200

    fix tests (untested)

--- src/github.com/sm0ken/screendream/video/playlist_test.go
+++ src/github.com/sm0ken/screendream/video/playlist_test.go
@@ -1,10 +1,9 @@
 package video
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
-
 
 func TestPlaylist(t *testing.T) {
 
@@ -20,33 +19,29 @@ func TestPlaylist(t *testing.T) {
 	vid3 := NewVideo("asdfb", "asdf", 46)
 	playlist.AddVideo(vid3)
 
-	fmt.Println("Current Playlist:\n"+playlist.string())
-
+	fmt.Println("Current Playlist:\n" + playlist.String())
 
 	// swapping videos
 	playlist.SwapVideos(vid1, vid2)
 
-	fmt.Println("Current Playlist:\n"+playlist.string())
-
+	fmt.Println("Current Playlist:\n" + playlist.String())
 
 	// deleting videos
 	playlist.RemoveVideo(vid2)
 
-	fmt.Println("Current Playlist:\n"+playlist.string())
+	fmt.Println("Current Playlist:\n" + playlist.String())
 
 	playlist.RemoveVideo(vid1)
 	playlist.RemoveVideo(vid3)
 
-	fmt.Println("Current Playlist:\n"+playlist.string())
+	fmt.Println("Current Playlist:\n" + playlist.String())
 
 }
-
 
 func TestVideoPrint(t *testing.T) {
 
 	vid := NewVideo("asdf", "asdf", 46)
 
-	fmt.Println("test:\n"+vid.string())
+	fmt.Println("test:\n" + vid.string())
 
 }
-
